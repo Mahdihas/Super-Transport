28c4baeb4a194f2c5ad116a537d058819db7c082
Mahdi Hasan <mahdihasan038@gmail.com>
2022-11-08 20:45:44 +0600

    designing my review page

--- src/component/My reviews/MyReviewCard.js
+++ src/component/My reviews/MyReviewCard.js
@@ -2,17 +2,28 @@ import React from 'react'
 
 const MyReviewCard = ({order,handleDelete}) => {
     const { _id, img,date,serviceName
-        ,        price,circle, name } = order;
+        ,    customer,message,    price,circle, name } = order;
   return (
-      <div>
+      <div className='sm:w-[80%] my-8 mx-auto'>
       
-  <div className="card-body">
-              <h2 className="card-title">{serviceName}</h2>
-              <img className='w-[40%]' src={img} alt="Shoes" />
-              <p>date : {date}</p>
-              <img src={circle} alt="img" />
-    <div className="card-actions justify-end">
-      <button onClick={() => handleDelete(_id)}  className="btn btn-primary text-white">Delete</button>
+  <div className="card-body  shadow-2xl">
+              <h2 className="card-title text-center text-slate-600"><p>Service Name : {serviceName}</p></h2>
+              <img className='w-[70%] mx-auto h-[200px]' src={img} alt="Shoes" />
+        <p className='px-12'>date : {date}</p>
+        <p className='px-12'>Price : <span className='text-yellow-600'>{price}$</span></p>
+
+        
+        <div className="flex px-12  justify-between items-center">
+          <div className="">        <img className='w-[60px] h-[60px] rounded-full' src={circle} alt="img" /></div>
+          <div className="">          <p className='p'>Customer Name : {customer}</p></div>
+
+        </div>
+        <p className='px-12 '><span className='text-yellow-500'>Comment</span> : {message}</p>
+
+    <div className="card-actions my-2 w-[80%] mx-auto justify-between">
+          <button onClick={() => handleDelete(_id)} className="btn btn-primary text-white">Delete</button>
+          <button onClick={() => handleDelete(_id)}  className="btn btn-primary text-white">Edit</button>
+
     </div>
   </div>
 
